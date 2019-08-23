e861b612c89dc11b7d151f3a8cfe26bd4f281a79
jameshowison <james@howison.name>
2019-08-23 12:13:56 -0500

    Clearing out for Fall 2019

--- class_scratch_space/pythonattempt.py
+++
@@ -1,8 +0,0 @@
-fname = open("/c/Users/nimis/peer_production_course/class_scratch_space/test_file.txt")
-words=0
-
-with open(fname, 'r') as f:
-	for line in f:
-		word = line.split()
-		words += len(word)
-print("number of words:",words)
